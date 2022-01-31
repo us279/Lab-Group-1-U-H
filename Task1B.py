aaf74b7781a6cad84d631538ef669dd255c4da0c
“henrywall56 <hjw61@cam.ac.uk>
2022-01-31 12:40:28 +0000

    1B finished

--- Task1B.py
+++ Task1B.py
@@ -1,7 +1,7 @@
-from cProfile import label
+
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
-from haversine import haversine, Unit
+
 
 stations=build_station_list()
 p= (52.2053, 0.1218)
@@ -9,19 +9,16 @@ p= (52.2053, 0.1218)
 station_distance = stations_by_distance(stations, p)
 n_t= []
 d= []
-for MonitoringStation in stations:
+n_t_d=[]
+for MonitoringStation, statdist in station_distance: # need to make sure list of stations is sorted by distance, so we can add individually element wise and make sure elements match up correctly
     name = MonitoringStation.name
     town = MonitoringStation.town
     n_t.append((name, town))
     for statdist in station_distance:
         distance = statdist[1]
         d.append(distance)
-n_t_d= []
-for i in range(len(n_t)):
-    n_t_d += (tuple(n_t[i])) + tuple(d) 
-print('test')
-
-
-
-
-
+n_t_d=list(zip(n_t, d))
+closest_stations = n_t_d[:10]
+furthest_stations = n_t_d[-10:]
+print('Closest 10 stations are:', closest_stations)
+print('Furthest 10 stations are:', furthest_stations)
