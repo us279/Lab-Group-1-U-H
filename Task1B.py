35912a936b7e24c4617bcbce9de08c805aba8a8a
“henrywall56 <hjw61@cam.ac.uk>
2022-01-31 11:13:03 +0000

    task1B

--- Task1B.py
+++ Task1B.py
@@ -1,7 +1,27 @@
+from cProfile import label
 from floodsystem.geo import stations_by_distance
 from floodsystem.stationdata import build_station_list
 from haversine import haversine, Unit
 
 stations=build_station_list()
-stations_and_centre = stations_by_distance(stations, (52.2053, 0.1218))
+p= (52.2053, 0.1218)
+
+station_distance = stations_by_distance(stations, p)
+n_t= []
+d= []
+for MonitoringStation in stations:
+    name = MonitoringStation.name
+    town = MonitoringStation.town
+    n_t.append((name, town))
+    for statdist in station_distance:
+        distance = statdist[1]
+        d.append(distance)
+n_t_d= []
+for i in range(len(n_t)):
+    n_t_d += (tuple(n_t[i])) + tuple(d) 
+print('test')
+
+
+
+
 
--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,9 +13,9 @@ from haversine import haversine, Unit
 
 def stations_by_distance(stations, p):      #takes list of stations and point p which we want to know the distance away from
     station_and_distance = []
-    for MonitoringStation in stations:          #for each individual station in list of stations
-        distance = haversine(MonitoringStation.coord, p)        #calculate distance from p
-        station_and_distance.append((MonitoringStation, distance))      #add tuple of station (along with all its data) and its distance from p to a collectiove list
+    for station in stations:          #for each individual station in list of stations
+        distance = haversine(station.coord, p)        #calculate distance from p
+        station_and_distance.append((station, distance))      #add tuple of station (along with all its data) and its distance from p to a collectiove list
     return sorted_by_key(station_and_distance, 1)       #return the list we just made, sorted by distance, the 2nd key in the tuple
 
 
