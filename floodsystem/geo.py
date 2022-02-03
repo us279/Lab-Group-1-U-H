79be77ccf625afa2696453d9d8818a403283a693
“henrywall56 <hjw61@cam.ac.uk>
2022-02-03 22:32:45 +0000

    1D geo functions

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,8 +7,11 @@ geographical data.
 """
 import math
 from math import sqrt
+
+from sympy import stationary_points
 from .utils import sorted_by_key  # noqa
 from haversine import haversine, Unit
+from floodsystem.stationdata import build_station_list
 
 #Task1B Henry Wall
 def stations_by_distance(stations, p):      #takes list of stations and point p which we want to know the distance away from
@@ -20,8 +23,7 @@ def stations_by_distance(stations, p):      #takes list of stations and point p 
 #need to run pytests for geo now
 
 #Task1C Henry Wall
-def stations_within_radius(stations, centre, r):
-    #want to return a list of stations within radius r of geographic coordinate x
+def stations_within_radius(stations, centre, r): #want to return a list of stations within radius r of geographic coordinate x
     close_stations=[]
     for station in stations:
         if  haversine(station.coord, centre) < r:
@@ -29,5 +31,37 @@ def stations_within_radius(stations, centre, r):
     return close_stations
             
 #Task 1D Henry Wall
+def rivers_with_station(stations): #want to return a list of rivers with stations
+    r_w_s = []
+    for i in stations: #for each monitoring station, add its river to a list, this is obviously a list of rivers with monitoring stations
+        r_w_s.append(i.river)
+    return set(r_w_s) #return a set so we get no repeats
+
+def stations_by_river(stations): #want to create a function that returns a dictionary mapping river names to a list of station objects along river
+    r_n_w_s = rivers_with_station(stations)
+    stations = build_station_list()
+    def stations_on_each_river(rivers, stations):     #need list of all stations on a river, listed for all rivers, create new function that returns this
+        stations_on_each_river_list = [[] for i in range(len(rivers))]
+        j=0
+        dict_element_list = []
+        for river_name in rivers:
+            for station in stations:
+                if river_name == station.name:
+                    stations_on_each_river_list[j].append(station)
+            dict_element_list.append({river_name: stations_on_each_river_list[j]})
+            j+=1
+
+        return stations_on_each_river_list #we are returning a list of dictionaries
+
+    dict_list = stations_on_each_river(r_n_w_s, stations)
+    dictionary = {}
+    for k in dict_list:
+        dictionary.update(k)
+    return dictionary
+
+
+
+
+               
 
 
