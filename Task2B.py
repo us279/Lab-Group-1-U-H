6a1f584ccd79d89173a3994760f1c0c4c47fc5d2
“henrywall56 <hjw61@cam.ac.uk>
2022-02-14 12:22:19 +0000

    2B test+yml

--- Task2B.py
+++ Task2B.py
@@ -3,15 +3,8 @@ from floodsystem.flood import stations_level_over_threshold
 from floodsystem.stationdata import build_station_list, update_water_levels
 stations = build_station_list()
 tol=0.8
-update_water_levels(stations)
+update_water_levels(stations) #Update all new water levels
 stations_over_threshold = stations_level_over_threshold(stations, tol)
 
 for tuple in stations_over_threshold:
     print(tuple[0].name, tuple[1])
-
-
-
-"Why?:"
-#for i, j in tuple:
-    #print(i.name, j)
-#TypeError: cannot unpack non-iterable MonitoringStation object
