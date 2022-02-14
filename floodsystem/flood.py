46ddfe8e31d34ae3e01e6cc91de03879bf6339af
“henrywall56 <hjw61@cam.ac.uk>
2022-02-14 13:39:15 +0000

    Task 2C, tests, yml

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -8,5 +8,22 @@ def stations_level_over_threshold(stations, tol):
             continue
         elif i.relative_water_level() > tol: #add stations with a relative water level above our tolerance as part of a tuple to a list
             s_l_o_t.append((i, i.relative_water_level()))
-    sorted_s_l_o_t = sorted(s_l_o_t, key=lambda x: x[1], reverse = True) #sort in descending order by second value in tuple
-    return sorted_s_l_o_t
+    s_l_o_t.sort( key=lambda x: x[1], reverse = True) #sort in descending order by second value in tuple
+    return s_l_o_t
+
+#2C Henry Wall
+def stations_highest_rel_level(stations, N):
+    "This function returns the first N rivers with the highest relative water levels"
+    stations_and_level = []
+    for station in stations:
+        if station.relative_water_level() == None: #skip stations with no or inconsistent data
+            continue
+        else:
+            stations_and_level.append((station, station.relative_water_level()))
+    stations_and_level.sort(key=lambda x: x[1], reverse=True) #Sort by relative water level in descending order
+    n=0
+    first_N_station_levels = []
+    while n<N:
+        first_N_station_levels.append(stations_and_level[n]) #add (station, relative water level) to list
+        n+=1
+    return first_N_station_levels
--- test_flood.py
+++ test_flood.py
@@ -1,13 +1,26 @@
 from numpy import float32
-from floodsystem.flood import stations_level_over_threshold
-from floodsystem.stationdata import build_station_list
+from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.station import MonitoringStation
 
 def test_stations_level_over_threshold():  #checking the output of the function is as intended
     stations = build_station_list()
+    update_water_levels(stations)
     over_threshold_list = stations_level_over_threshold(stations, tol=0.8)
     assert isinstance(over_threshold_list, list) 
     for i in over_threshold_list:
+        assert isinstance(i, tuple)
+        assert isinstance(i[0], MonitoringStation)
+        assert isinstance(i[1], float)
+    
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+    update_water_levels(stations)
+    N=10
+    first_10_stations = stations_highest_rel_level(stations, N)
+    assert isinstance(first_10_stations, list)
+    assert len(first_10_stations) == 10
+    for i in first_10_stations:
         assert isinstance(i, tuple)
         assert isinstance(i[0], MonitoringStation)
         assert isinstance(i[1], float)
