645cd66ec7e9c6231f6871482420f20a4b12a35f
Uday Singh <us279@cam.ac.uk>
2022-02-20 23:00:02 +0000

    Fix of 2F

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -30,21 +30,22 @@ def plot_water_level_with_fit(station, dates, levels, p):
         polynomial, d0 = polyfit(dates, levels, p)
         stations = build_station_list()
         #This section of code returns evenly spaced points of typical range over the time period dates
-        for station in stations:
-            if station.name == station:
-                typical_low=np.linspace(station.typical_range[0],station.typical_range[0],len(dates))
-                typical_high=np.linspace(station.typical_range[1],station.typical_range[1],len(dates))
+        for i in stations:
+            if i.name == station:
+                typical_low=np.linspace(i.typical_range[0],i.typical_range[0],len(dates))
+                typical_high=np.linspace(i.typical_range[1],i.typical_range[1],len(dates))
         
         #Plots
-        plt.plot(dates, levels)
-        plt.plot(dates, typical_low)
-        plt.plot(dates, typical_high)
+        plt.plot(dates,levels)
+        plt.plot(dates, polynomial(np.array(matplotlib.dates.date2num(dates))-d0))
+        plt.plot(dates,typical_low)
+        plt.plot(dates,typical_high)
 
         # Add axis labels, rotate date labels and add plot title
         plt.xlabel('date')
         plt.ylabel('water level (m)')
         plt.xticks(rotation=45);
-        plt.title("Water level for station :"+ stations + "in the last 10 days")
+        plt.title(station)
 
         #Display plot
         plt.tight_layout()  # This makes sure plot does not cut off date labels
