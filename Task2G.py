5501f0077b8cb996be521b50750be7a4712ee66b
Uday Singh <us279@cam.ac.uk>
2022-02-21 23:06:44 +0000

    Except TypeError 2G

--- Task2G.py
+++ Task2G.py
@@ -10,12 +10,11 @@ severe_risk = []
 high_risk = []
 moderate_risk = []
 low_risk = []
+
 #This section of code collects information from the function water_level in analysis and allocates it to the respective list
 low, mid, high = water_level()
 
-
-#In this section of code for every station in list 'high' it computes rate of increase of water level and assesses the risk of flodding in that station depending on rate of increase.
-#The expressions except ignores any value that has an error due to being out of range or is missing a value for water level
+#In this section of code for every station in list 'high' it computes rate of increase of water level and assesses the risk of flodding in that station depending on rate of increase. #The expressions except ignores any value that has an error due to being out of range or is missing a value for water level
 for station in high:
     try:
         dt = 1
@@ -28,9 +27,12 @@ for station in high:
         else:
             moderate_risk.append(station.name)
     except IndexError:
-        pass
+        pass 
     except KeyError:
         pass
+    except TypeError:
+        pass
+
 print("Number of stations at severe risk:")
 print(len(severe_risk))
 print("Number of stations at high risk: ")
@@ -41,20 +43,24 @@ print("Number of stations at low risk: ")
 print(len(low_risk))
 #In this section of code for every station in list 'mid' it computes rate of increase of water level and assesses the risk of flodding in that station depending on rate of increase.
 #The expressions except ignores any value that has an error due to being out of range or is missing a value for water level
-for station in mid:
+for stations in mid:
     try:
         dt = 1
-        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        dates, levels = fetch_measure_levels(stations.measure_id, dt=datetime.timedelta(days=dt))
         rate_of_increase_mid = rate_of_increase(dates, levels, 1)
-        if rate_of_increase_mid > 2:
-            high_risk.append(station.name)
-        elif rate_of_increase_mid >1:
-            moderate_risk.append(station.name)
+        if rate_of_increase_mid > 3:
+            severe_risk.append(stations.name)
+        elif rate_of_increase_mid >2:
+            high_risk.append(stations.name)
+        elif rate_of_increase_mid>1:
+            moderate_risk.append(stations.name)
         else:
-            low_risk.append(station.name)
+            low_risk.append(stations.name)
     except IndexError:
         pass
     except KeyError:
+        pass
+    except TypeError:
         pass
 print("Number of stations at severe risk:")
 print(len(severe_risk))
@@ -65,21 +71,24 @@ print(len(moderate_risk))
 print("Number of stations at low risk: ")
 print(len(low_risk))
 
-#In this section of code for every station in list 'low' it computes rate of increase of water level and assesses the risk of flodding in that station depending on rate of increase.
-#The expressions except ignores any value that has an error due to being out of range or is missing a value for water level
-for station in low:
+#In this section of code for every station in list 'low' it computes rate of increase of water level and assesses the risk of flodding in that station depending on rate of increase. #The expressions except ignores any value that has an error due to being out of range or is missing a value for water level
+
+for stations in low:
     try:
         dt = 1
-        dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+        dates, levels = fetch_measure_levels(stations.measure_id, dt=datetime.timedelta(days=dt))
         rate_of_increase_low = rate_of_increase(dates, levels, 1)
-        if rate_of_increase_high == np.float64:
-            if rate_of_increase_low > 2:
-                moderate_risk.append(station.name)
-            else:
-                low_risk.append(station.name)
+        if rate_of_increase_low > 3:
+            high_risk.append(stations.name)
+        elif rate_of_increase_low >2:
+            moderate_risk.append(stations.name)
+        else:
+            low_risk.append(stations.name)
     except IndexError:
         pass
     except KeyError:
+        pass
+    except TypeError:
         pass
 
 print("Number of stations at severe risk:")
